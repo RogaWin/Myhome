3424ca6cc7287c7ac39b55ba3be995aae4d1d984
luojiawei <2240774934@qq.com>
2024-07-28 20:41:44 +0800

    Create a EasyReader

---
+++ big-dick-front/src/views/article/EasyReader.vue
@@ -0,0 +1,16 @@
+<template>
+    <MdPreview :editorId="id" :modelValue="text" />
+    <MdCatalog :editorId="id" :scrollElement="scrollElement" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { MdPreview, MdCatalog } from 'md-editor-v3';
+// preview.css相比style.css少了编辑器那部分样式
+import 'md-editor-v3/lib/preview.css';
+
+const id = 'preview-only';
+const text = ref('# Hello Editor' +
+    'Hello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello EditorHello Editor');
+const scrollElement = document.documentElement;
+</script>
